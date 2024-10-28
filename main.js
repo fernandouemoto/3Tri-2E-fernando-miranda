2ce0d8d46022faebfd3915c9363b9d9768e0e798
fernandouemoto <160140614+fernandouemoto@users.noreply.github.com>
2024-10-28 08:54:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
     function criarCartao (categoria,pergunta,resposta){
-        console.log(categoria,pergunta,resposta)
+    let container = document.getElementById('container')
+    let article = document.createElement('article')
+    article.className = 'cartao'
+    article.innerHTML = 'isso é um teste'
+    container.appendChild(article)
 }
-criarCartao('Biologia','Qual a funçâo da mitocondria')
+
+
+criarCartao('Biologia','Qual a funçâo da mitocondria','Produção de energia(ATP)')
+
+calculaIMC(80,1.65)
