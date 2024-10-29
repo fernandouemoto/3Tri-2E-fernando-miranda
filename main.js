4a2d09671ccd416512f33cbac3c7545c8a9a7e01
fernandouemoto <160140614+fernandouemoto@users.noreply.github.com>
2024-10-29 08:51:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,19 @@
     let container = document.getElementById('container')
     let article = document.createElement('article')
     article.className = 'cartao'
-    article.innerHTML = 'isso é um teste'
+    article.innerHTML = `
+        <div class="cartao-conteudo">
+            <h3> ${ categoria} </h3>
+            <div class="cartao-conteudo-pergunta">
+                <p> ${pergunta} </p> 
+            </div>
+            <div class="cartao-conteudo-resposta">
+                <p> ${resposta} </p>
+            </div>
+        </div>
+    `
     container.appendChild(article)
 }
 
-
 criarCartao('Biologia','Qual a funçâo da mitocondria','Produção de energia(ATP)')
-
-calculaIMC(80,1.65)
+criarCartao('Matematica','Como é definido um grupo?','é um conjunto de individuos que reunidos formam um todo')
